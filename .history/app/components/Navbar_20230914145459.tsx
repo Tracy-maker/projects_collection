c42d19d1e199240ec1f62020dbcbf5ca188d1326
Tracy-maker <ydlvns@gmail.com>
2023-09-14 14:58:08 +1000

    add the side bar css

--- .history/app/components/Navbar_20230914145459.tsx
+++ .history/app/components/Navbar_20230914145459.tsx
@@ -102,7 +102,7 @@ export default function Navbar() {
                 className={`${
                   pathname === "/"
                     ? "bg-blue-50 border-blue-500 text-blue-500  block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
-                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-white block pl-3 pr-4 py-2 dark:hover:bg-gray-700"
+                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 dark:hover:bg-gray-700"
                 }`}
               >
                 HOME
@@ -114,8 +114,8 @@ export default function Navbar() {
                 prefetch
                 className={`${
                   pathname === "/guestFeedback"
-                    ? "bg-blue-50 border-blue-500 text-blue-500  block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
-                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-white block pl-3 pr-4 py-2  dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
+                    ? "border-blue-500 text-blue-500  block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
+                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2  dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
                 }`}
               >
                 Feedback
@@ -127,8 +127,8 @@ export default function Navbar() {
                 prefetch
                 className={`${
                   pathname === "/projects"
-                    ? "bg-blue-50 border-blue-500 text-blue-500  block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
-                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-white block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
+                    ? "border-blue-500 text-blue-500  block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
+                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
                 }`}
               >
                 Projects
