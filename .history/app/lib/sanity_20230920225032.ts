b3e9273412cc927da588e877d6487b80d5de9008
Tracy-maker <ydlvns@gmail.com>
2023-09-20 22:52:37 +1000

    fix version

--- .history/app/lib/sanity_20230920225032.ts
+++ .history/app/lib/sanity_20230920225032.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 const projectId = "14m7i8qn";
 const dataset = "production";
-const apiVersion = "2023-01-01";
+const apiVersion = "2023-10-17";
 
 export const client = createClient({
   projectId,
