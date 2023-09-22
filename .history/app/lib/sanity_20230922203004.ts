503720f10e91111b8f3b4929be6f5c0263651174
Tracy-maker <ydlvns@gmail.com>
2023-09-22 20:31:07 +1000

    upload the new project

--- .history/app/lib/sanity_20230922203004.ts
+++ .history/app/lib/sanity_20230922203004.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 const projectId = "14m7i8qn";
 const dataset = "production";
-const apiVersion = "2023-10-01";
+const apiVersion = "2023-11-01";
 
 export const client = createClient({
   projectId,
