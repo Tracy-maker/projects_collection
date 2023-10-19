e1d8b566f9418abd415edc3c86420694b6555402
Tracy-maker <ydlvns@gmail.com>
2023-10-19 13:06:23 +1100

    add the data

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 const projectId = "14m7i8qn";
 const dataset = "production";
-const apiVersion = "2023-10-31";
+const apiVersion = "2023-10-25";
 
 export const client = createClient({
   projectId,
